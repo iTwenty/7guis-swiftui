ac46be39556493f6370f5075081a6a2ea8a2b42c
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-05-08 17:06:11 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jaydeep Joshi on 08/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/seven_guisApp.swift
@@ -0,0 +1,17 @@
+//
+//  seven_guisApp.swift
+//  Shared
+//
+//  Created by Jaydeep Joshi on 08/05/22.
+//
+
+import SwiftUI
+
+@main
+struct seven_guisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
